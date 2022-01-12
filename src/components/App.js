af7dc8dfaae48d4dc2c30a525e2270c134f31c59
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-01-12 10:07:19 +0530

    Add Ipod Container which is the main body of Ipod

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import '../App.css';
+import Ipod from './Ipod'
 
 function App() {
   return (
     <div className="App">
-      
+      <Ipod/>
     </div>
   );
 }
---
+++ src/components/Ipod.js
@@ -0,0 +1,32 @@
+import React from 'react';
+class Ipod extends React.Component{
+    constructor(){
+        // We have to call the constructor of parent class that is super() as we are inheriting state constructor in our parent constructor
+        super();
+    }
+
+    render = () => {
+        return(
+            <div style = {styles.ipodCase}>
+                
+            </div>
+        );
+    }
+}
+
+// Style of ipod case
+const styles = {
+    ipodCase : {
+        height : '21rem',
+        width : '18rem',
+        backgroundColor : 'black',
+        margin : 'auto',
+        // We will display ipod items as flex and direction as coloumn
+        display : 'flex',
+        flexDirection : 'coloumn',
+        justifyContent : 'center'
+
+    }
+}
+
+export default Ipod;
