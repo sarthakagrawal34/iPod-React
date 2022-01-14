b898ecd3702f662d333e64dcfe69798f2cb25658
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-01-14 09:47:21 +0530

    Complete My Music Screen

--- src/components/MyMusic.js
+++ src/components/MyMusic.js
@@ -5,31 +5,57 @@ class MyMusic extends React.Component{
 
     constructor(){
         super();
+        this.state = {
+            audio : null
+        }
     }
 
 
     componentDidMount(){
+        let self = this;
         const audioEl = document.getElementsByClassName("audio-element")[0];
-        audioEl.play();
+        // audioEl.play();
+        self.audio = audioEl;
+        self.audio.play();
+
+        self.audio.addEventListener("timeupdate",function(){
+            var pos = audioEl.currentTime/audioEl.duration;
+            self.updateTime();
+             document.getElementById("fill").style.width = pos*100 + "%";
+        })
+    }
+
+    updateTime = () =>{
+        this.setState({
+            audio : this.audio
+        })
     }
 
     render(){
+        let audio = this.state.audio;
         return(
             <div style={styles.myMusicContainer}>
 
                 <div style={styles.info}>
                     <img style={styles.image} src="https://www.guinnessworldrecords.com/Images/despacito%20header_tcm25-520895.jpg"></img>
                     <div style={styles.subInfo}>
-                        <h4>Song Name</h4>
-                        <h5>Song Name</h5>
+                        {/* <h4>Song Name</h4>
+                        <h5>Song Name</h5> */}
+                        <h4 style={{marginBottom:'0.5rem'}}>Senorita</h4>
+                        <p style={{marginBottom:'0'}}>Camilla Cobello</p>
+                        <p>Shawn Mendes</p>
                     </div>
-
                 </div>
 
                 <div style={styles.statusBar}>
                     <audio className="audio-element">
                         <source src={sound}></source>
                     </audio>
+                    <p style={styles.currTime}>{audio != null ? Math.floor(audio.currentTime) : 0 / 0}</p>
+                    <div style={styles.seekBar}>
+                        <div style={styles.fill} id='fill'></div>
+                    </div>
+                    <p style={styles.dur}>{audio != null ? Math.floor(audio.duration) : null}</p>
                 </div>
 
             </div>
@@ -59,9 +85,33 @@ const styles = {
     },
     statusBar : {
         width : '100%',
-        height : '30%'
+        height : '30%',
+        display : 'flex',
+        flexDirection : 'row',
+        flexWrap : 'wrap',
+        justifyContent : 'space-evenly'
     },
     subInfo : {
+        alignSelf : 'center'
+    },
+    seekBar : {
+        width:'70%',
+        height:'20%',
+        border : '1px solid grey',
+        cursor: 'pointer',
+        alignSelf : 'center',
+        display: 'flex',
+    },
+    fill : {
+        height: '100%',
+        backgroundColor: 'royalblue',
+    },
+    currTime : {
+        margin : '0',
+        alignSelf : 'center'
+    },
+    dur : {
+        margin : '0',
         alignSelf : 'center'
     }
 }
